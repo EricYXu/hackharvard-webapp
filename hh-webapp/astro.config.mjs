92855106ee3eb3aa7869415fc15f7733d865fbad
Eric Xu <eiyxu03@gmail.com>
2025-08-20 21:07:34 -0400

    Added more pages about rules, updates, scheduling, and reference

--- hh-webapp/astro.config.mjs
+++ hh-webapp/astro.config.mjs
@@ -2,25 +2,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'HackHarvard Handbook',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hackharvard' }],
-			sidebar: [
-				{
-					label: 'Announcements',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Organizer Updates', slug: 'guides/announcements' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'HackHarvard Handbook',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hackharvard' }],
+        sidebar: [
+            {
+                label: 'LOOK HERE FOR UPDATES',
+                items: [
+                    { label: 'Day 1 (10/03) Updates', slug: 'updates/announcements_10-03' },
+                    { label: 'Day 2 (10/04) Updates', slug: 'updates/announcements_10-04' },
+                    { label: 'Day 3 (10/05) Updates', slug: 'updates/announcements_10-05' }
+                ],
+            },
+            {
+                label: 'Rules and Regulations',
+                items: [
+                    { label: 'Hacker Rules', slug: 'rules/ground_rules' },
+                    { label: 'Project Rules', slug: 'rules/project_rules' },
+                    { label: 'Judging Criteria', slug: 'rules/judging_criteria' }
+                ],
+            },
+            {
+                label: 'Schedule',
+                items: [
+                    { label: 'Day 1 Schedule', slug: 'schedule/day1_schedule' },
+                    { label: 'Day 2 Schedule', slug: 'schedule/day2_schedule' },
+                    { label: 'Day 3 Schedule', slug: 'schedule/day3_schedule' }
+                ],
+            },
+            {
+                label: 'Reference',
+                items: [
+                    { label: 'Shuttles', slug: 'reference/shuttles' },
+                ]
+            },
+        ],
+		}), mdx()],
 });
